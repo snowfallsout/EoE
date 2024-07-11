e519cf6ced632bbfc216119b30c4a24dea5b09e9
Xueyi Huang <729792791@qq.com>
2024-07-11 15:27:32 +0100

    08

---
+++ assets/register_types-30d06446.js
@@ -0,0 +1,1 @@
+import{Behaviour as e,TypeStore as o}from"./needle-engine-1ec926cf.js";import"./three.module-1012b49f.js";import"./index-7f496757.js";class a extends e{start(){console.log("Hello")}update(){const t=new THREE.Vector3(0,1,0);this.gameObject.rotateOnWorldAxis(t,this.context.time.deltaTime*-.5)}}o.add("Ratate",a);
